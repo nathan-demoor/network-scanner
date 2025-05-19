add3bf30b0566e9b4d3b978789e73b21072ef641
cixk1 <72662310+cixk1@users.noreply.github.com>
2025-05-19 23:16:08 +0200

    working socket connection

---
+++ netscan.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    // target machine IP otherwise specify via the arguments
+    char target_ip[] = "127.0.0.1";
+    int status, client_fd;
+
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (client_fd == -1) {
+        printf("Could not create a socket\n");
+        return -1;
+    }
+
+    struct sockaddr_in my_addr;
+    my_addr.sin_family = AF_INET;
+    // Port is defined here
+    my_addr.sin_port = htons(8080);
+    
+    // IPv4 address is defined here
+    if (inet_pton(AF_INET, target_ip, &my_addr.sin_addr) <= 0) {
+        printf("Invalid address\n");
+        return -1;
+    }
+
+    if (status = connect(client_fd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
+        printf("Connection failed\n");
+        return -1;
+    } else {
+        printf("Connection success\n");
+    }
+
+    close(client_fd);
+    return 0;
+}
