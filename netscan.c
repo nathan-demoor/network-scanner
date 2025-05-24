c91b3f53f72e8e3c65e17af45dd43c89ede7c21b
cixk1 <72662310+cixk1@users.noreply.github.com>
2025-05-24 14:18:36 +0200

    scanning multiple ports

--- netscan.c
+++ netscan.c
@@ -6,50 +6,72 @@
 #include <arpa/inet.h>
 
 int scan_single_port(char ip[],  int port) {
-    int client_fd;
-
-    client_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (client_fd < 0) {
-        printf("Could not create a socket\n");
-        return -1;
-    }
-
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in my_addr;
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(port);
+
+    if (fd < 0) {
+        printf("Could not create a socket\n");
+        return -1;
+    }
     
     if (inet_pton(AF_INET, ip, &my_addr.sin_addr) <= 0) {
         printf("Invalid address\n");
         return -1;
     }
 
-    if (connect(client_fd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
-        printf("Connection failed\n");
-        return -1;
+    if (connect(fd, (struct sockaddr*)&my_addr, sizeof(my_addr)) == 0) {
+        printf("Connection success port: %d\n", port);
     }
-    printf("Connection success\n");
 
-    close(client_fd);
+    close(fd);
     return 0;
 }
 
-int scan_all_ports(int ip) {
-    int MAX_PORT = 65535;
-    return 0; 
+int scan_all_ports(char ip[]) {
+    int MAX_PORT = 65535, status; // 65535
+
+    for (int i = 1; i < MAX_PORT; i++) {
+        status = scan_single_port(ip, i);
+    }
+
+    return status; 
+}
+
+void help(int argc, char *argv[]) {
+    printf("Usage: %s -u IP [-p PORT] [-m] MODE ((S) Single | (M) Multiple)\n", argv[0]);
 }
 
 int main(int argc, char *argv[]) {
-    char target_ip[16];
-    int port, status;
+    char *target_ip = malloc(100);
+    char mode;
+    int port, status, opt;
 
-    // Do mode selection first before asking for any values
-
-    printf("Please enter an IP address: ");
-    scanf("%s", &target_ip);
-    printf("Please enter an IP port: ");
-    scanf("%d", &port);
+     while ((opt = getopt(argc, argv, "u:p:m:")) != -1) {
+        switch (opt) {
+            case 'u':
+                target_ip = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'm':
+                mode = *optarg;
+                if (mode == 'S') {
+                    status = scan_single_port(target_ip, port);
+                 } else if (mode == 'M') {
+                    status = scan_all_ports(target_ip);
+                } else {
+                    printf("Incorrect mode specified\n");
+                    return 1;
+                }
+                break;
+            default:
+                help(argc, argv);
+                return 1;
+        }
+    }
     
-    status = scan_single_port(target_ip, port);
     return status;
 }
